6bb4500199bd2904dfb167dcb4e481f413945c3e
RandomRamen <pt072093@gmail.com>
2021-01-21 19:58:03 -0600

    add node implementation

---
+++ LinearDS/Node.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, value, previous_node = None, next_node = None):
+        self.value = value
+        self.previous_node = previous_node
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next_node(self):
+        return self.link_node
+
+    def set_value(self, new_value):
+        self.value = new_value
+
+    def set_next_node(self, new_node):
+        self.next_node = new_node
+
+    def set_previous_node(self, new_node):
+        self.previous_node = new_node
---
+++ script.py
@@ -0,0 +1,4 @@
+from LinearDS import Node
+
+test_node = Node.Node("This is a new node.")
+print(test_node)
